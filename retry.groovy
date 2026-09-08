f975972851d560b9ad476e0676abe1b19826a307
Dima <96681581+DimaRT-ED@users.noreply.github.com>
2026-09-08 20:40:00 +0300

    Create retry.groovy

---
+++ retry.groovy
@@ -0,0 +1,35 @@
+pipeline {
+    agent { label 'master'}
+    options {
+        timeout(time: 10, unit: 'MINUTES')
+        timestamps()  // Add timestamps to console output
+    }
+    environment {
+        OWNER = "Dima"
+        PROJECT = "Jenkins course"
+    }
+
+    stages {
+        parallel {
+            stage('Read file 2.txt') {
+                steps {
+                    retry(3) {
+                        sh '''
+                            sleep 3
+                            ls -la
+                            cat 2.txt
+                        '''
+                    }
+                }
+            }
+            stage('Create file 2.txt') {
+                steps {
+                    sh'''
+                        sleep 5
+                        echo "HELLO FROM STAGE 2" >> 2.txt
+                    '''
+                }
+            }
+        }
+    }
+}
